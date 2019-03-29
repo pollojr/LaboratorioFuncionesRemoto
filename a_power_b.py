ca27cfb135f35be742564610805a2b227a756489
Facultad de Ing Sistemas e informatica <informatica@bga.upb>
2019-03-29 13:55:05 -0500

    cambios

--- a_power_b.py
+++ a_power_b.py
@@ -2,10 +2,19 @@ def a_power_b (a , b):
     prod = 1
     for i in range(0, b):
         prod=prod*a
-    print(prod)
+    return(prod)
 
-num1=int(input("digite un numero"))
-num2=int(input("digite la potencia a elevar el numero"))        
-a_power_b(num1,num2)
+while True:
 
+    a=int(input("Digite un numero:"))
+    if a==0:
+        print("numero es cero")
+        break
+    b=int(input("Digite el numero a elevar:"))
+    result=a_power_b(a,b)
+    print("el resultado es: ",result)
+    if a==0:
+        break
+
+a_power_b(a,b)
      
